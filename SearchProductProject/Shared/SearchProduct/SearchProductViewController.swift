18dcb0488bde927ab76fbd25cfbfc840077c6c9e
Mahika Hosanee <mahika.hosanee@zalora.com>
2022-04-23 15:20:50 +0800

    Create base search product screen

---
+++ SearchProductProject/Shared/SearchProduct/SearchProductViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchProductViewController.swift
+//  SearchProductProject
+//
+//  Created by Mahika on 23/04/2022.
+//
+
+import Foundation
+import UIKit
+
+class SearchProductViewController: UIViewController {
+    required init() {
+        super.init(nibName: SearchProductViewController.className, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
