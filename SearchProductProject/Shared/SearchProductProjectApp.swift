3a77d88317dd6889718896697eedb2bea8738d95
Mahika Hosanee <mahika.hosanee@zalora.com>
2022-04-23 13:56:44 +0800

    Create base project for Search Products

---
+++ SearchProductProject/Shared/SearchProductProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchProductProjectApp.swift
+//  Shared
+//
+//  Created by Mahika on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchProductProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
