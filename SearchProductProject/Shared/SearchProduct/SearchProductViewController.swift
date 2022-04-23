356639ba01a545a7b00737f56fafdaf344f56f99
Mahika Hosanee <mahika.hosanee@zalora.com>
2022-04-23 16:40:30 +0800

    Add shadows, corner radius and borders to search view

--- SearchProductProject/Shared/SearchProduct/SearchProductViewController.swift
+++ SearchProductProject/Shared/SearchProduct/SearchProductViewController.swift
@@ -9,11 +9,31 @@ import Foundation
 import UIKit
 
 class SearchProductViewController: UIViewController {
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var closeImageView: UIImageView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var shadowView: UIView!
+    
     required init() {
         super.init(nibName: SearchProductViewController.className, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        containerView.layer.cornerRadius = 5
+        containerView.layer.borderWidth = 1
+        containerView.layer.borderColor = UIColor.grey60.cgColor
+        searchButton.layer.cornerRadius = 5
+        searchButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        shadowView.addBottomShadow()
     }
 }
