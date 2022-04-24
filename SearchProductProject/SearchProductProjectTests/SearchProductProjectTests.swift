c8e0f18a92f05180cfabc7b16d4712ffb640efdd
Mahika Hosanee <mahika.hosanee@zalora.com>
2022-04-24 14:25:52 +0800

    Configure new unit test target for project

---
+++ SearchProductProject/SearchProductProjectTests/SearchProductProjectTests.swift
@@ -0,0 +1,14 @@
+//
+//  SearchProductProjectTests.swift
+//  SearchProductProjectTests
+//
+//  Created by Mahika on 24/04/2022.
+//
+
+import XCTest
+
+class SearchProductProjectTests: XCTestCase {
+
+    func testExample() {}
+
+}
